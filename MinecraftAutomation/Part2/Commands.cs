ad05f58439795b5ac3ee1dbe8a886eede75980dd
takunology <m15025@g.metro-cit.ac.jp>
2020-10-26 22:13:31 +0900

    Part2

---
+++ MinecraftAutomation/Part2/Commands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    /// <summary>
+    /// Minecraft に投げるコマンド
+    /// </summary>
+    public class Commands : Rcon
+    {
+        public async Task Teleport(string playerName)
+        {
+            await rcon.ConnectAsync();
+            string result = await rcon.SendCommandAsync($"/tp {playerName} ~ ~ ~");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MinecraftAutomation/Part2/Program.cs
@@ -0,0 +1,23 @@
+﻿using CoreRCON;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            Console.WriteLine("Start!");
+
+            //コマンド用インスタンス
+            Commands mcCommand = new Commands();
+
+            await mcCommand.Teleport("Takunology");
+        }
+    }
+
+
+}
