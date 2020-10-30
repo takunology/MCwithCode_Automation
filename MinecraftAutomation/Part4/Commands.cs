30288e83e858baa1ef1e153c81a6c8211447f487
takunology <m15025@g.metro-cit.ac.jp>
2020-10-30 16:39:10 +0900

    Part4

---
+++ MinecraftAutomation/Part4/Commands.cs
@@ -0,0 +1,176 @@
+﻿using CoreRCON.Parsers.Standard;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Part4
+{
+    /// <summary>
+    /// Minecraft に投げるコマンド
+    /// </summary>
+    public class Commands : Rcon
+    {
+        public float PlayerPosX;
+        public float PlayerPosY;
+        public float PlayerPosZ;
+
+        public async Task Teleport(string playerName, float x, float y, float z)
+        {
+            await rcon.ConnectAsync();
+            string result = await rcon.SendCommandAsync($"/tp {playerName} {x} {y} {z}");
+            Console.WriteLine(result);
+        }
+
+        public async Task GetPosition(string playerName)
+        {
+            await rcon.ConnectAsync();
+            // Minecraft 1.13 以降では data コマンドで座標を得られる
+            // 1.12 以前では tp ~ ~ ~ で現在地へテレポートして座標を得る
+            string result = await rcon.SendCommandAsync($"/data get entity {playerName} Pos");
+            string filterResult = Regex.Replace(result, @"[^0-9-,.]", "");
+            string[] splitResult = filterResult.Split(",");
+
+            PlayerPosX = float.Parse(splitResult[0]);
+            PlayerPosY = float.Parse(splitResult[1]);
+            PlayerPosZ = float.Parse(splitResult[2]);
+        }
+
+        public async Task SetBlock(float x, float y, float z, string blockName)
+        {
+            await rcon.ConnectAsync();
+            string result = await rcon.SendCommandAsync($"/setblock {x} {y} {z} {blockName}");
+            Console.WriteLine(result);
+        }
+
+        public async Task SetIchimatsu(int x, int y, int z, int range)
+        {
+            await rcon.ConnectAsync();
+            
+            for(int i = 0; i < value; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    for (int j = 0; j < value; j++)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            string result = await rcon.SendCommandAsync($"/setblock {x + i} {y} {z + j} minecraft:oak_planks");
+                            Console.WriteLine(result);
+                        }
+                        else
+                        {
+                            string result = await rcon.SendCommandAsync($"/setblock {x + i} {y} {z + j} minecraft:spruce_planks");
+                            Console.WriteLine(result);
+                        }
+                    }
+
+                }
+                else
+                {
+                    for (int j = 0; j < value; j++)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            string result = await rcon.SendCommandAsync($"/setblock {x + i} {y} {z + j} minecraft:spruce_planks");
+                            Console.WriteLine(result);
+                        }
+                        else
+                        {
+                            string result = await rcon.SendCommandAsync($"/setblock {x + i} {y} {z + j} minecraft:oak_planks");
+                            Console.WriteLine(result);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        public async Task NetherGate()
+        {
+            await rcon.ConnectAsync();
+
+            int x = (int)PlayerPosX + 3;
+            int y = (int)PlayerPosY;
+            int z = (int)PlayerPosZ;
+
+            string result;
+
+            //黒曜石を配置
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    result = await rcon.SendCommandAsync($"/setblock {x} {y + j} {z + i} minecraft:obsidian");
+                    Console.WriteLine(result);
+                }
+            }
+
+            //空気ブロック配置
+            for (int i = 1; i < 3; i++)
+            {
+                for (int j = 1; j < 4; j++)
+                {
+                    result = await rcon.SendCommandAsync($"/setblock {x} {y + j} {z + i} minecraft:air");
+                    Console.WriteLine(result);
+                }
+            }
+            //火をつける
+            result = await rcon.SendCommandAsync($"/setblock {x} {y + 1} {z + 2} minecraft:fire");
+            Console.WriteLine(result);
+        }
+
+        public async Task CreatePond(int range)
+        {
+            await rcon.ConnectAsync();
+
+            int x = (int)PlayerPosX + 2;
+            int y = (int)PlayerPosY;
+            int z = (int)PlayerPosZ;
+
+            string result;
+
+            // 2×2以上のみ受け付ける
+            if (range < 4)
+                return;
+
+            //石ブロックの配置
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < range; j++)
+                {
+                    for(int k = 0; k < range; k++)
+                    {
+                        result = await rcon.SendCommandAsync($"/setblock {x + j} {(y - 1) + i} {z + k} minecraft:stone");
+                        Console.WriteLine(result);
+                    }
+                }
+            }
+
+            //中身をくり抜く
+            for(int i = 1; i < range - 1; i++)
+            {
+                for(int j = 1; j < range - 1; j++)
+                {
+                    result = await rcon.SendCommandAsync($"/setblock {x + i} {y} {z + j} minecraft:air");
+                    Console.WriteLine(result);
+                }
+            }
+
+            //水源を配置
+            for(int i = 1; i < range - 1; i++)
+            {
+                for (int j = 1; j < range - 1; j++)
+                {
+                    if(i == j)
+                    {
+                        result = await rcon.SendCommandAsync($"/setblock {x + i} {y} {z + j} minecraft:water");
+                        Console.WriteLine(result);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MinecraftAutomation/Part4/Program.cs
@@ -0,0 +1,24 @@
+﻿using CoreRCON;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Part4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //コマンド用インスタンス
+            Commands command = new Commands();
+            await command.GetPosition("Takunology");
+
+            //プレイヤーが押し出されるのを防ぐ
+            int x = (int)command.PlayerPosX + 1;
+            int y = (int)command.PlayerPosY;
+            int z = (int)command.PlayerPosZ;
+
+            await command.CreatePond(8);
+        }
+    }
+}
