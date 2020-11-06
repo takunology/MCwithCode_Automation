a90f462acbe26e19dd7b2bc5d59d99d955ac0ee1
takunology <m15025@g.metro-cit.ac.jp>
2020-11-06 22:38:52 +0900

    Part7

---
+++ MinecraftAutomation/Part7/OutFiles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Part7
+{
+    // アイテム（ブロック）の座標を出力する
+    public class OutFiles
+    {
+        public void OutOreCoordinate(List<string> Ores)
+        {
+            string path = $@".\Output\coordinate.txt";
+            try
+            {
+                StreamWriter sw = new StreamWriter(path, false, Encoding.GetEncoding("utf-8"));
+                foreach(string ore in Ores)
+                {
+                    sw.WriteLine(ore);
+                }
+                sw.Close();
+                Console.WriteLine($"\nCoordinate File : {path}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ MinecraftAutomation/Part7/Program.cs
@@ -0,0 +1,27 @@
+﻿using CoreRCON;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Part7
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //コマンド用インスタンス
+            Commands command = new Commands();
+            await command.GetPosition("Takunology");
+
+            int x = (int)command.PlayerPosX + 1;
+            int y = (int)command.PlayerPosY;
+            int z = (int)command.PlayerPosZ;
+
+            await command.GroundLeveling(x, y, z, x + 100, y + 2, z + 100);
+
+            //await command.SetTorch(x, z, x + 100, z + 100);
+        }
+    }
+}
